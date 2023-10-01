72d4fadff7bb0960e8d9c3130975225a1b683dff
Samuel <sam.saravia2010@gmail.com>
2023-10-01 16:22:44 -0600

    feat: agregando funciones de 1 a la 3 de la tarea

---
+++ JS/01-Ejercicios.js
@@ -0,0 +1,32 @@
+// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+const getCharacter=(cadena="")=>cadena.length;
+console.log(getCharacter("hola"));
+
+// 2)Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+const getSizeCharacter=(cadena="",cantidadLetras=1)=>cadena.slice(0,cantidadLetras);
+console.log(getSizeCharacter("hola",2));
+
+// 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+const getArrayDelimiter =(cadena="",delimitador)=>cadena.split(delimitador);
+console.log(getArrayDelimiter("hola como estas"," "));
+
+// 4) Programa una fucion para que calcule el precio de un producto aplicando un descuento, pe. miFuncion(1000, 20) devolverá 800.
+// 5) Calcular la edad de una persona, pe. miFuncion(new Date(1988,09,01)) devolverá 34 años.
+// 6) Programa que convierte decimal a romano hasta el número 1000.  mifuncion(15)  devolver:  el 15 es XV
+// 7) progrma conviete decimal a binario. ejemplo: 1000 = 1111101000.
+// 8) Programa que devuelva el número de vocales de una cadena de texto. Ejemplo: "Hola Mundo" = 4 vocales.
+// 9) Programa que valide si una palabra es palíndromo o no. Ejemplo: "Salas" = true, "Hola" = false.
+// 10) Programa que elimine los espacios de una cadena de texto. Ejemplo: "Hola Mundo" = "HolaMundo".
+// 11) Programa que tome la primera letra de cada palabra y lo devuelva en mayúsculas. Ejemplo: "Yoni Escobar". Resultado: "YE".
+// 12) Programa que devuelva la cantidad de palabras de una cadena de texto. Ejemplo: "Hola Mundo" = 2 palabras.
+// 13) Programa que devuelva el texto con la primera letra de cada palabra en mayúscula. Ejemplo: "hola mundo" = "Hola Mundo".
+// 14) Programa que devuelva la cadena de texto invertida. Ejemplo: "Hola Mundo" = "odnuM aloH".
+// 15) Programa que pase un arreglo de números y devuelva la suma de todos los números. Ejemplo: suma(10, 20, 30, 40, 50) = "La suma de todos los números es 150".
+// 16) Programa que pase un arreglo de edades y devuelva la mayor y la menor. Ejemplo: edades(10, 20, 30, 40, 50) = "La edad mayor es 50 y la edad menor es 10".
+// 17) programa de escala de notas de 0 a 10. Ejemplo: 0-5 = "Pésimo", 6-7 = "Regular", 8-9 = "Bueno", 10 = "Excelente".
+// 18) programa que devuelva el número de cifras de un número entero. Ejemplo: 1234 = 4 cifras.
+// 19) programa que muestre los numeros primos de 1 a 1000.
+// 20) Programa que realice el juego de piedra, papel o tijera. Ejemplo: "Piedra" > "Tijera", "Tijera" > "Papel", "Papel" > "Piedra".
+
+// //Resultado: juegoPiedraPapelTijera("piedra", "papel") = "Gana papel".
+// 21) Programa con temporizador que muestre un mensaje cada cierto tiempo. Ejemplo: "Hola Mundo" cada 3 segundos.
